a9b7e1a0b1616259c87f77449c647cc5c2fdd26f
Nagananda K K <naganandakk@yahoo.com>
2015-01-30 23:37:19 +0530

    set_data.js

---
+++ set_data.js
@@ -0,0 +1,24 @@
+
+var json_data = [];
+var json_transactions = [];
+
+function set_transactions ( data ) {
+	for ( var index = 0; index < data.length; index++ ) {
+		json_transactions[index] = {
+			"date":data[index][0],
+			"money":data[index][1],
+			"type":data[index][2]
+		};
+	}
+
+}
+
+function set_data ( data ) {
+	for ( var index = 0; index < data.length; index++ ) {
+		json_transactions[index] = {
+			"date":data[index][0],
+			"money":data[index][1]
+		};
+	}
+
+}
